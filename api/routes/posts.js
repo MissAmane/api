19d499ec5c3f7d0579e3e8cd03abb05147f0512f
MissAmane <mansion.pe@gmail.com>
2023-11-30 00:05:02 -0500

    Se agrega el districtValue para ser devuelto en el listado de posts

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,47 +1,54 @@
-const express = require('express');
-const Posts = require('../models/Posts');
-const { isAuthenticated, hasRoles } = require('../auth');
+const express = require("express");
+const Posts = require("../models/Posts");
+const { isAuthenticated, hasRoles } = require("../auth");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   Posts.find(
     { status: true },
-    { name: 1, nationality: 1, images: 1, rate_1h: 1, verified: 1 }
+    {
+      name: 1,
+      nationality: 1,
+      images: 1,
+      rate_1h: 1,
+      verified: 1,
+      districtValue: 1,
+    }
   )
     .exec()
     .then((x) => res.status(200).send(x));
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   Posts.findById(req.params.id)
     .exec()
     .then((x) => res.status(200).send(x));
 });
 
-router.get('/me/:uuid', (req, res) => {
+router.get("/me/:uuid", (req, res) => {
   Posts.find({ uuid: req.params.uuid })
     .exec()
     .then((x) => res.status(200).send(x));
 });
 
-router.post('/', isAuthenticated, (req, res) => {
+router.post("/", isAuthenticated, (req, res) => {
   Posts.create({ ...req.body }).then((x) =>
     res.status(201).json({
       status: 201,
       data: null,
-      message: 'Post creado con exito',
+      message: "Post creado con exito",
     })
   );
 });
 
-router.put('/:id', isAuthenticated, hasRoles(['admin', 'user']), (req, res) => {
-  Posts.findByIdAndUpdate({_id: req.params.id}, req.body).then((x) =>
+router.put("/:id", isAuthenticated, hasRoles(["admin", "user"]), (req, res) => {
+  Posts.findByIdAndUpdate({ _id: req.params.id }, req.body).then((x) =>
     res.sendStatus(204)
   );
 });
 
-router.delete('/:id', isAuthenticated, (req, res) => {
+router.delete("/:id", isAuthenticated, (req, res) => {
   Posts.findOneAndDelete(req.params.id)
     .exec()
     .then(() => res.sendStatus(204));
