866f31d10be879c6cb44f65715c40f7c0e752c3c
MissAmane <mansion.pe@gmail.com>
2024-04-21 14:51:15 -0500

    add vip key on get posts

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -8,7 +8,7 @@ router.get("/", async (req, res) => {
   const limit = req.query.limit || 30;
   const page = req.query.page || 1;
   const select =
-    "name nationality images profession rate_1h verified districtValue age alwaysOn days iniTime endTime";
+    "name nationality images profession rate_1h verified vip districtValue age alwaysOn days iniTime endTime";
 
   const posts = await Posts.paginate(
     { status: true },
